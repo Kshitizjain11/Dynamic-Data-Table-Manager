f4de1777690d08b1df759f0c5bb87973dcaf69f6
Kshitiz <kshitizjain2020@gmail.com>
2025-11-05 15:53:47 +0530

    feat:Added react redux and implemented initial store setup

---
+++ src/providers/ReduxProvider.tsx
@@ -0,0 +1,12 @@
+import { store } from '@/store/store'
+import React from 'react'
+import { Provider } from 'react-redux'
+
+export const ReduxProvider = ({children}:{children: React.ReactNode}) => {
+  return (
+    <Provider store={store}>
+        {children}
+    </Provider>
+  )
+}
+
